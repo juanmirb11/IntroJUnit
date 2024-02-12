1f6b105614b16d6fe87ee1f7e6b1117dcc1fe2c5
Juanmi <>
2024-02-12 14:52:05 +0100

    Afegit contingut a main i creada la classe "MoneyUtil".

---
+++ src/main/java/com/mycompany/introjunit/IntroJUnit.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.introjunit;
+
+/**
+ *
+ * @author Juanmi
+ */
+public class IntroJUnit {
+
+    public static void main(String[] args) {
+        double salary = 1683.781;
+        System.out.println(MoneyUtil.format(salary));
+    }
+
+    class MoneyUtil {
+
+        public static String format(double money) {
+            return "$" + money;
+        }
+    }
+}
